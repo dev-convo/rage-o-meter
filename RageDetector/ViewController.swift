1e048e00cec9874d70c8e6f6d5683f8fa9895270
Bilal Mughal <bilal.mughal@convo.com>
2022-05-20 10:52:47 +0500

    Initial Commit

---
+++ RageDetector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RageDetector
+//
+//  Created by Bilal Mughal on 20/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
